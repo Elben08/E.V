4b5e12fe5b8d5ba68d236ecb14b2ecde61734f62
Elben08 <myvideosonly2023@gmail.com>
2026-08-19 16:15:22 +0800

    v47: Replace dashboard card mic emoji with FB Messenger-style SVG

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ const DEFAULT_SETTINGS = {
   macroWebhook: ''
 };
 
-const APP_VERSION = 'v46';
+const APP_VERSION = 'v47';
 
 function cap(text) {
   return text.charAt(0).toUpperCase() + text.slice(1);
