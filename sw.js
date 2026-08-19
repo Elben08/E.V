8b5c774b54f183a2c4785b62bc09f693ae541d1c
Elben08 <myvideosonly2023@gmail.com>
2026-08-19 16:31:23 +0800

    v50: Remove book icon from chat topbar, freeze header on scroll

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ev-cache-v49';
+const CACHE = 'ev-cache-v50';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
